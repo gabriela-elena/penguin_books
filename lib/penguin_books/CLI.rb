7edfd6c2106c35185b9dd8e3dabc0bd7393cecab
Gabriela Ruvalcaba <gabrielaelenadesigns@gmail.com>
2020-12-23 01:17:13 -0800

    start cli stub

--- lib/penguin_books/CLI.rb
+++ lib/penguin_books/CLI.rb
@@ -1,6 +1,23 @@
 class PenguinBooks::CLI
     def call 
         puts 'Welcome to Penguin Books!'
-        binding.pry
+        author_spotlight
+        new_releases
+        # popular
+        # upcoming_for(preorders)
+        # list_authors
     end
+
+    def author_spotlight
+        # to be scraped instead
+        @authors = ['Sabaa Tahir', 'Ricky Yancey', 'Alison Goodman']
+    end
+
+    def new_releases
+        # list new books
+        @authors.each.with_index(1) do |author, index| 
+            puts "#{index}. #{author}"
+    end
+end
+
 end
