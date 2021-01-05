aafeba08a7a9f14be5dd8e34e78829e3f41043fc
Gabriela Ruvalcaba <gabrielaelenadesigns@gmail.com>
2021-01-04 16:56:16 -0800

    jan 4

--- lib/penguin_books/scraper.rb
+++ lib/penguin_books/scraper.rb
@@ -2,19 +2,25 @@
 class PenguinBooks::Scraper
     def self.scrape_authors
         doc = Nokogiri::HTML(open("https://www.penguinrandomhouse.com/the-read-down/trending-this-week/"))
-        
-        authors = doc.css("ol.awesome-list li.inner-facade")
-        #binding.pry
-        #or authors = doc.css("class#inner-facade")?
-        authors.each do |a|
+        container = doc.css("ol.awesome-list") #grab the container containing all books
+        books = container.css('li.inner-facade') # select all li's that contain individual book info
+        #or authorsq = doc.css("class#inner-facade")?
+        books.each do |a|
             binding.pry
-            name = a.text
+            author_name = a["author"]
+            book_title = a["ttl"]
+            book_category = a["cat-data"]
+            book_summary = a.css('div.desc').text.strip
+            # name = a.text
             PenguinBooks::Author.new(name)
         end
     end
+    
     def self.scrape_author(book)
         PenguinBooks::Book.new("newest books", author)
         PenguinBooks::Book.new("previous book", author)
     end
+  
 end
+
 
